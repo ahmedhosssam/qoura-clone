fb435ee8832b9232457bcabdc24aa07429d78386
ahmedhossam <hoss4614@gmail.com>
2023-02-17 07:15:17 +0200

    finish header jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,42 @@
+import QouraLogo from '../../assets/Quora.svg.png';
+import homePage from '../../assets/home-svgrepo-com.svg';
+import list from '../../assets/list.svg';
+import answer from '../../assets/answer.svg';
+import spaces from '../../assets/spaces.svg';
+import notification from '../../assets/notification.svg';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="left-header">
+        <img src={QouraLogo} alt="qoura" className="header-qoura-logo" />
+        <div className="header-left-buttons">
+          <button className="header-button">
+            <img src={homePage} />
+          </button>
+          <button className="header-button">
+            <img src={list} />
+          </button>
+          <button className="header-button">
+            <img src={answer} />
+          </button>
+          <button className="header-button">
+            <img src={spaces} />
+          </button>
+          <button className="header-button">
+            <img src={notification} />
+          </button>
+        </div>
+      </div>
+
+      <input placeholder="🔍 Search Qoura" />
+      <div className="right-header">
+        <div className="profile-pic-header-container">
+          <img src />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom/client';
 // Components
 import Header from './Components/Header/Header';
 
+// Styles
+import './Styles/App.scss';
+import './Styles/header.scss';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
